44743a39e5b3fbc405bc88f903f84c1d46e95e1a
robot12138 <lyice188@163.com>
2021-10-13 00:51:05 +0800

    修复初版bug

--- src/views/robotTool.vue
+++ src/views/robotTool.vue
@@ -84,8 +84,13 @@
                       <div>
                         <el-form :label-position="labelPosition" :rules="rules" ref="ruleForm" label-width="80px" :model="ruleForm">
                           <el-form-item label="开始时间" prop="startTime">
-                            <el-time-select v-model="ruleForm.startTime" :picker-options="startPickerOptions" placeholder="选择开始时间">
-                            </el-time-select>
+
+                            <el-time-picker
+                              v-model="ruleForm.startTime"
+                              placeholder="任意时间点">
+                            </el-time-picker>
+                            <!-- <el-time-select v-model="ruleForm.startTime" :picker-options="startPickerOptions" placeholder="选择开始时间">
+                            </el-time-select> -->
                           </el-form-item>
                           <el-form-item label="开始延迟" prop="advance">
                             <el-input v-model.number="ruleForm.advance"></el-input>
@@ -119,8 +124,8 @@
                         <span>日志</span>
                         <el-button style="float: right" size="small" @click="clearLog" type="danger">清空</el-button>
                       </div>
-                      <div v-for="log in logs" :key="log.id" class="text item">
-                        {{log.account}}:{{log.count}}{{log.log}},时间：{{log.diff}}
+                      <div v-for="log in logs" :key="log.id" class="text item" style="color:#409EFF;font-size:13px;margin-bottom:5px;">
+                        [{{log.account}}]:{{log.count}}:{{log.log}},开始时间:{{log.startReqTime}},返回时间:{{log.endReqTime}},耗时:{{log.diff}}ms
                       </div>
                     </el-card>
                   </el-col>
@@ -145,113 +150,99 @@
 </style>
 
 <script>
-import { now } from 'moment';
+import { now } from "moment";
 // @ is an alias to /src
 import { Common } from "../model/common";
 import { Jd } from "../model/jd";
+import { setTimeout } from "timers";
 let common = new Common();
 let jd = new Jd();
-//日期格式化
-Date.prototype.Format = function (formatStr) {
-  var str = formatStr;
-  var Week = ['日', '一', '二', '三', '四', '五', '六'];
-
-  str = str.replace(/yyyy|YYYY/, this.getFullYear());
-  str = str.replace(/yy|YY/, (this.getYear() % 100) > 9 ? (this.getYear() % 100).toString() : '0' + (this.getYear() % 100));
-
-  str = str.replace(/MM/, this.getMonth() > 8 ? (this.getMonth() + 1).toString() : '0' + (this.getMonth() + 1));
-  str = str.replace(/M/g, this.getMonth());
-
-  str = str.replace(/w|W/g, Week[this.getDay()]);
-
-  str = str.replace(/dd|DD/, this.getDate() > 9 ? this.getDate().toString() : '0' + this.getDate());
-  str = str.replace(/d|D/g, this.getDate());
-
-  str = str.replace(/hh|HH/, this.getHours() > 9 ? this.getHours().toString() : '0' + this.getHours());
-  str = str.replace(/h|H/g, this.getHours());
-  str = str.replace(/mm/, this.getMinutes() > 9 ? this.getMinutes().toString() : '0' + this.getMinutes());
-  str = str.replace(/m/g, this.getMinutes());
-
-  str = str.replace(/ss|SS/, this.getSeconds() > 9 ? this.getSeconds().toString() : '0' + this.getSeconds());
-  str = str.replace(/s|S/g, this.getSeconds());
-
-  str = str.replace(/xx|XX/, this.getMilliseconds());
-
-  return str;
-};
 export default {
   name: "Home",
   data() {
     return {
       currentTime: "",
-      clock: 0,//开始倒计时
+      preStartUp: false, //预先启动，降低冷启动、并发实例及业务代码初始化引起的耗时
+      clock: 0, //开始倒计时
       count: [],
       timer: [],
-      isStart: false,//是否开始
+      isStart: false, //是否开始
       showInfo: "",
-      jdCouponInterval: 0,//京东定时器
+      jdCouponInterval: 0, //京东定时器
       startPickerOptions: {
-        start: '00:00',
-        step: '01:00',
-        end: '23:59:59'
+        start: "00:00",
+        step: "01:00",
+        end: "23:59:59"
       },
       tableData: [],
-      labelPosition: 'right',
+      labelPosition: "right",
       ruleForm: {
-        url: "https://api.m.jd.com/client.action?functionId=newBabelAwardCollection&body={%22activityId%22:%222QwmJao59JSGzjWtEWsT5zgxk291%22,%22scene%22:1,%22args%22:%22key=gcu1i1d2ef24012f42bc7549e9a99be2,roleId=60257525,strengthenKey=C225838DC1351F9ACA8548E3171FF8A4A79652C3B40AA10EA6208B56F12A9AF708B8A2FA5F5B50B95A49BC155D64D3A4_babel%22}&client=wh5",
+        url:
+          "https://api.m.jd.com/client.action?functionId=newBabelAwardCollection&body={%22activityId%22:%222QwmJao59JSGzjWtEWsT5zgxk291%22,%22scene%22:1,%22args%22:%22key=gcu1i1d2ef24012f42bc7549e9a99be2,roleId=60257525,strengthenKey=C225838DC1351F9ACA8548E3171FF8A4A79652C3B40AA10EA6208B56F12A9AF708B8A2FA5F5B50B95A49BC155D64D3A4_babel%22}&client=wh5",
         startTime: "",
         count: 1,
         step: 300,
         advance: -200
       },
       rules: {
-        url: [{ required: true, message: '地址不能为空' }],
-        startTime: [{ required: true, message: '开始时间不能为空' }],
-        advance: [{ required: true, message: '提前时间不能为空' }, { type: 'number', message: '提前时间必须为数字值' }],
-        step: [{ required: true, message: '间隔时间不能为空' }, { type: 'number', message: '间隔时间必须为数字值' }],
-        count: [{ required: true, message: '领取次数不能为空' }, { type: 'number', message: '领取次数必须为数字值' }]
+        url: [{ required: true, message: "地址不能为空" }],
+        startTime: [{ required: true, message: "开始时间不能为空" }],
+        advance: [
+          { required: true, message: "提前时间不能为空" },
+          { type: "number", message: "提前时间必须为数字值" }
+        ],
+        step: [
+          { required: true, message: "间隔时间不能为空" },
+          { type: "number", message: "间隔时间必须为数字值" }
+        ],
+        count: [
+          { required: true, message: "领取次数不能为空" },
+          { type: "number", message: "领取次数必须为数字值" }
+        ]
       },
       accountForm: {
         name: "",
         value: ""
       },
       accountRules: {
-        value: [{ required: true, message: '账户不能为空' }]
+        value: [{ required: true, message: "账户不能为空" }]
       },
       logs: []
     };
   },
   computed: {
-    btnTxt: function () {
-      return this.isStart ? '停止' : '开始'
+    btnTxt: function() {
+      return this.isStart ? "停止" : "开始";
     }
   },
   components: {},
   created() {
-    if (window.localStorage.getItem('account')) {
-      this.tableData = JSON.parse(localStorage.getItem('account'))
+    if (window.localStorage.getItem("account")) {
+      this.tableData = JSON.parse(localStorage.getItem("account"));
     }
-    if (window.localStorage.getItem('setting')) {
-      this.ruleForm = JSON.parse(localStorage.getItem('setting'))
+    if (window.localStorage.getItem("setting")) {
+      this.ruleForm = JSON.parse(localStorage.getItem("setting"));
     }
     //this.init()
   },
   methods: {
     //开始
     submitRulesForm(formName) {
-      this.$refs[formName].validate((valid) => {
-        console.log(valid)
+      this.$refs[formName].validate(valid => {
+        console.log(valid);
         if (valid) {
+          this.preStartUp = false;
           this.count = [];
           this.timer = [];
           if (this.isStart) {
             this.isStart = false;
-            this.clock && window.clearTimeout(this.clock)
+            this.clock && window.clearTimeout(this.clock);
+            console.log(this.clock);
           } else {
-            this.init()
+            this.init();
           }
         } else {
-          console.log('error submit!!');
+          console.log("error submit!!");
           return false;
         }
       });
@@ -262,13 +253,13 @@ export default {
     },
     handleDelete(index, row) {
       console.log(index, row);
-      this.tableData.splice(index, 1)
+      this.tableData.splice(index, 1);
     },
     //添加账号
     addAccount(formName) {
       let _this = this;
-      this.$refs[formName].validate((valid) => {
-        console.log(valid)
+      this.$refs[formName].validate(valid => {
+        console.log(valid);
         if (valid) {
           let info = _this[formName];
           _this.tableData.push({
@@ -278,7 +269,7 @@ export default {
           });
           this.$refs[formName].resetFields();
         } else {
-          console.log('error submit!!');
+          console.log("error submit!!");
           return false;
         }
       });
@@ -286,79 +277,120 @@ export default {
     //保存消息
     saveAccountInfo() {
       if (this.tableData.length == 0) {
-        window.localStorage.removeItem('account')
+        window.localStorage.removeItem("account");
       } else {
-        window.localStorage.setItem('account', JSON.stringify(this.tableData))
+        window.localStorage.setItem("account", JSON.stringify(this.tableData));
       }
     },
     //保存设置
     setting() {
-      window.localStorage.setItem('setting', JSON.stringify(this.ruleForm))
+      window.localStorage.setItem("setting", JSON.stringify(this.ruleForm));
     },
     async init() {
       try {
         let _this = this;
         if (_this.tableData.length == 0) {
           _this.$message.error({
-            message: '请检查账户信息', type: 'error'
+            message: "请检查账户信息",
+            type: "error"
           });
-          return
+          return;
         }
         _this.isStart = true;
         let res = await common.getJdTime();
         let now = new Date().getTime();
         this.currentTime = now - parseInt(res.currentTime2);
-        let h = this.ruleForm.startTime.substr(0, this.ruleForm.startTime.indexOf(':'));
-        let m = this.ruleForm.startTime.substr(this.ruleForm.startTime.indexOf(':') + 1);
-        var starttime = new Date(parseInt(res.currentTime2));//服务器时间
+        let time = this.ruleForm.startTime;
+        let h = time.getHours();
+        let m = time.getMinutes();
+        let s = time.getSeconds();
+        var starttime = new Date(parseInt(res.currentTime2)); //服务器时间
         var hours = starttime.setHours(h);
         var min = starttime.setMinutes(m);
-        var sec = starttime.setSeconds(0);
+        var sec = starttime.setSeconds(s);
         var count = 0;
         var interval = 50;
-        let preStartUp = false;//预先启动，降低冷启动、并发实例及业务代码初始化引起的耗时
+
         //var misec = starttime.setMilliseconds(ms || 0);
-        starttime = new Date(new Date(starttime).getTime() + this.ruleForm.advance);
-        console.log(starttime)
+        starttime = new Date(
+          new Date(starttime).getTime() + this.ruleForm.advance
+        );
+        console.log(starttime);
         var localNowtime = new Date();
-        var difference = localNowtime.getTime() - parseInt(res.currentTime2);//时间差
+        var difference = localNowtime.getTime() - parseInt(res.currentTime2); //时间差
         var starttimes = starttime.getTime();
-        let _interval = function () {
+        let _interval = function() {
+          let _self = this;
           count++;
-          _this.clock && window.clearTimeout(_this.clock)
-          let nowtime1 = new Date();//获取当前时间
+          _this.clock && window.clearTimeout(_this.clock);
+          let nowtime1 = new Date(); //获取当前时间
           let offset = nowtime1.getTime() - (now + count * interval);
           let nowtimes = nowtime1.getTime() - difference; //校对当前时间
           //'开始(毫秒):'+parseInt(starttimes) ,'当前(毫秒):'+parseInt(nowtimes),
-          console.log('%c' + parseInt((starttimes) - parseInt(nowtimes)) / 1000 + '秒后开始,时间差:' + difference + 'ms', 'color: #7ED0A7;')
-          _this.showInfo = parseInt((starttimes) - parseInt(nowtimes)) / 1000 + '秒后开始,时间差:' + difference + 'ms';
+          console.log(
+            "%c" +
+              parseInt(starttimes - parseInt(nowtimes)) / 1000 +
+              "秒后开始,时间差:" +
+              difference +
+              "ms",
+            "color: #7ED0A7;"
+          );
+          _this.showInfo =
+            parseInt(starttimes - parseInt(nowtimes)) / 1000 +
+            "秒后开始,时间差:" +
+            difference +
+            "ms";
           //$('#dingshiEd').removeAttr('disabled').removeClass('layui-btn-disabled');
-          if ((parseInt(starttimes) - parseInt(nowtimes)) <= 0) {
-            _this.showInfo = '已开始自动抢券';
-            console.log('已开始自动抢券')
+          if (parseInt(starttimes) - parseInt(nowtimes) <= 0) {
+            _this.showInfo = "已开始自动抢券";
+            console.log("已开始自动抢券");
             //_this.jdCouponInterval = setInterval(_this.getJdCoupon, _this.ruleForm.step); //n毫秒查询一次库存,单位毫秒 自己根据情况修改
-            _this.clock && window.clearTimeout(_this.clock)
+            _this.clock && window.clearTimeout(_this.clock);
             _this.startGetCouponInterval();
             //console.log(_this.clock)
-            return
-          } else if ((parseInt(starttimes) - parseInt(nowtimes)) <= 10000) {
-            if (!preStartUp) {
-              preStartUp = true;
-              _this.getJdCoupon({ value: 'pre-start-up' }, 0, 0);
-            }
+            return;
+          } else if (parseInt(starttimes) - parseInt(nowtimes) <= 10000) {
+            _this.preStartUpInterval();
           }
-          //_interval()
-          _this.clock = setTimeout(function () {
-            _interval()
-          }, interval - offset)
+
+          _this.clock = window.setTimeout(() => {
+            _interval();
+          }, interval - offset);
           //console.log(_this.clock)
-        }
-        _interval()
-
+        };
+        _interval();
       } catch (error) {
-        this.$message.error({ message: error, type: 'error' });
+        this.$message.error({ message: error, type: "error" });
       }
-
+    },
+    //预先请求，解决并发问题。
+    preStartUpInterval() {
+      let _this = this;
+      if (!_this.preStartUp) {
+        _this.preStartUp = true;
+        console.log("preStartUpInterval");
+        let canUseData = 1;
+        _this.tableData.forEach(el => {
+          if (!el.disabled) {
+            canUseData++;
+          }
+        });
+        let preMax = _this.ruleForm.count * canUseData;
+        console.log(preMax);
+        let preCount = 0;
+        let _preInterval = function() {
+          preCount++;
+          if (preCount > preMax) {
+            preTimer && clearTimeout(preTimer);
+            return;
+          }
+          _this.getJdCoupon({ value: "pre-start-up" }, 0, 0);
+          let preTimer = setTimeout(function() {
+            _preInterval();
+          }, _this.ruleForm.step / canUseData);
+        };
+        _preInterval();
+      }
     },
     startGetCouponInterval() {
       // if (this.count > this.ruleForm.count) {
@@ -367,47 +399,67 @@ export default {
       // }
       let _this = this;
       let max = this.ruleForm.count;
-      let _interval = function (el, index) {
+      let _interval = function(el, index) {
         let count = _this.count[index] || 0;
         count++;
-        _this.$set(_this.count, index, count)
+        _this.$set(_this.count, index, count);
         //console.log('%ccoupon:' + el + ' ' + count[index] + '/' + max + ' ' + 'jd time:' + new Date(new Date().getTime() - currentTime).Format("MM-dd HH:mm:ss.xx") + ' ' + rdata.subCodeMsg, 'color:#B086D5;')
         if (count > max) {
           _this.timer[index] && clearTimeout(_this.timer[index]);
-          return false
+          return false;
         }
         _this.getJdCoupon(el, index, count);
         let timer = setTimeout(() => {
-          _interval(el, index)
-        }, _this.ruleForm.step)
-        _this.$set(_this.timer, index, timer)
-      }
+          _interval(el, index);
+        }, _this.ruleForm.step);
+        _this.$set(_this.timer, index, timer);
+      };
       this.tableData.forEach((el, index) => {
         // _this.count.push(0)
         // _this.timer.push(0)
-        _interval(el, index)
-      })
+        _interval(el, index);
+      });
     },
     getJdCoupon(el, index, count) {
       let _this = this;
-      let moment = require('moment');
+      let moment = require("moment");
       if (!el.disabled) {
-        jd.getCoupon({
-          url: _this.ruleForm.url,
-          ck: el.value
-        }).then((res) => {
-          let diff = moment(new Date().getTime() - parseInt(_this.currentTime)).format("HH:mm:ss:SSS");
-          _this.logs.push({ 'account': 'No.' + (index + 1), 'count': '第' + count + '次', 'log': JSON.stringify(res), id: index + "" + count + "" + new Date().getTime(), diff: diff })
-        }).catch((error) => {
-          let diff = moment(new Date().getTime() - parseInt(_this.currentTime)).format("HH:mm:ss:SSS");
-          _this.logs.push({ 'account': 'No.' + (index + 1), 'count': '第' + count + '次', 'log': JSON.stringify(error), id: index + "" + count + "" + new Date().getTime(), diff: diff })
-        })
+        let startReqTime = new Date().getTime() - parseInt(_this.currentTime);
+        jd
+          .getCoupon({
+            url: _this.ruleForm.url,
+            ck: el.value
+          })
+          .then(res => {
+            let endReqTime = new Date().getTime() - parseInt(_this.currentTime);
+            _this.logs.push({
+              account: "No." + (index + 1),
+              count: "第" + count + "次",
+              log: JSON.stringify(res),
+              id: index + "" + count + "" + new Date().getTime(),
+              startReqTime: moment(startReqTime).format("HH:mm:ss:SSS"),
+              endReqTime: moment(endReqTime).format("HH:mm:ss:SSS"),
+              diff: endReqTime - startReqTime
+            });
+          })
+          .catch(error => {
+            let endReqTime = new Date().getTime() - parseInt(_this.currentTime);
+            _this.logs.push({
+              account: "No." + (index + 1),
+              count: "第" + count + "次",
+              log: JSON.stringify(error),
+              id: index + "" + count + "" + new Date().getTime(),
+              startReqTime: moment(startReqTime).format("HH:mm:ss:SSS"),
+              endReqTime: moment(endReqTime).format("HH:mm:ss:SSS"),
+              diff: endReqTime - startReqTime
+            });
+          });
       }
     },
-    clearLog() {
-      this.logs = []
-    }
 
+    clearLog() {
+      this.logs = [];
+    }
   }
 };
 </script>
